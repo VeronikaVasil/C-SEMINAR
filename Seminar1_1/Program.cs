99aaa51b32a4319f48eedf067608f3193f71ea0a
Veronika <valeria14081981@yandex.com>
2022-08-06 01:30:59 +0300

    C

--- Seminar1_1/Program.cs
+++ Seminar1_1/Program.cs
@@ -1,6 +1,37 @@
 ﻿// Вводит три целых числа и выдаёт максимальное из этих чисел//;
-Console.WriteLine("Введите три числа");
+int a, b, c;
 
+Console.WriteLine("Введите три числа :");
+a = Convert.ToInt32(Console.ReadLine());
+ 
+b = Convert.ToInt32(Console.ReadLine());
+
+c = Convert.ToInt32(Console.ReadLine());
+ 
+   if (b < a && a > c)
+
+     {
+
+       Console.WriteLine(a);
+    
+     }
+
+     else if (a < b && b > c)
+     {
+
+       Console.WriteLine(b);
+    
+     }
+
+     else if (a < c && c > b)
+     {
+
+     Console.WriteLine(c);
+    
+
+     }
+      
+     Console.ReadLine();
 
 
 
