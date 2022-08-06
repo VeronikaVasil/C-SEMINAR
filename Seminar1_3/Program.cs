5bc0638a1b4654bf68674075cc6a7e685187cf97
Veronika <valeria14081981@yandex.com>
2022-08-06 14:59:51 +0300

    Определяет четные числа

--- Seminar1_3/Program.cs
+++ Seminar1_3/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿  class Program
+ 
+    {
+ 
+        static void Main(string[] args)
+ 
+        {
+ 
+            int i;
+ 
+            Console.Write("Введите число : ");
+ 
+            i = int.Parse(Console.ReadLine());
+ 
+            if (i % 2 == 0)
+ 
+            {
+ 
+                Console.Write("Введенное число является четным");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("Введенное число нечетное");
+ 
+                Console.Read();
+ 
+            }
+ 
+        }
+ 
+    }
+ 
+ 
