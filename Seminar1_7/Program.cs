d60cbcf694aa28fc5030e93912269e04afe36b5c
Veronika <valeria14081981@yandex.com>
2022-08-09 00:52:13 +0300

    3 цифра заданного числа

--- Seminar1_7/Program.cs
+++ Seminar1_7/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести третью цифру заданного числа или сообщить, что третьей цифры нет.
+
+int a;
+
+Console.Write("Введите число:" );
+
+
+a = Convert.ToInt32(Console.ReadLine());
+
+
+    if (a < 100)
+
+        {
+            Console.Write("Третьей цифры нет");
+        }
+
+     else if (a > 100)
+         
+        {
+            string str = a.ToString();
+            Console.Write("{0} третья цифра", (str[2]));
+        }
+
+
+    
+
+
+
+
+
+
