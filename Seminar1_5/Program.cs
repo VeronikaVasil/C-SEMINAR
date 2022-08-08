eb383642b615b778dfccf55c2813941c8b7954ac
Veronika <valeria14081981@yandex.com>
2022-08-08 20:59:00 +0300

    Zadacha

---
+++ Seminar1_5/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine();
+int number = new Random().Next(100, 1000);
+int first1 = number/100;
+int second2 = number % 10;
+int result = first1*10 + second2;
+Console.WriteLine(result);
+
+
+
