756a45c01c76236b4140acce30b7464c917f4f44
Veronika <valeria14081981@yandex.com>
2022-08-08 21:28:15 +0300

    Кратно или нет

---
+++ Seminar1_6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Write prog 2
+Console.WriteLine("Input number1: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number2: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int remain = (number1 % number2);
+
+if (remain == 0)
+{
+    Console.WriteLine ("кратно");
+}
+else
+{
+    Console.WriteLine ("некратно"+ number1 % number2)
+}
