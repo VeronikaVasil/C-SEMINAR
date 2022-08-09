20a592e5f56c041b2146585e657b9855e6095fe4
Veronika <valeria14081981@yandex.com>
2022-08-09 04:17:23 +0300

    Seminar2_1

---
+++ Seminar2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// The program outputs a random number from the segment and show max
+// [10,99]
+
+int number = new Random().Next(10,100);
+//[10;99]указать 100 вместо 99
+Console.WriteLine($"Random: {number}"); //Интерполяция
+// camelCase
+int firstDigit = number / 10;
+
+int secondDigit = number % 10;
+
