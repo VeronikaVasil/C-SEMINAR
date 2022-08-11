adc5520c493c95ab7a048c2edd917be91046e861
Veronika <valeria14081981@yandex.com>
2022-08-12 00:02:07 +0300

    Таблица кубов чисел

---
+++ Seminar3_5/Program.cs
@@ -0,0 +1,14 @@
+﻿ //Задача 22: Напишите программу, которая принимает на вход число (N) 
+ // и выдаёт таблицу кубов чисел от 1 до N. 
+ 
+ Console.WriteLine("Введите целое положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+     while (count <= N)
+    {
+       Console.Write($"{count * count * count} ");
+       count += 1;
+    }
+
+
