8194faef5bbacf9b0933a95b5c2b2e8d4e1053fc
Veronika <valeria14081981@yandex.com>
2022-08-11 22:49:05 +0300

    Диапазон по номеру координат

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 18: Напишите программу, которая по заданному  
+// номеру четверти, показывает диапазон возможных  
+// координат точек в этой четверти (x и y).  
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(quarter); 
+if (quarter == 1) Console.WriteLine("x > 0 && y > 0"); 
+if (quarter == 2) Console.WriteLine("x < 0 && y > 0"); 
+if (quarter == 3) Console.WriteLine("x < 0 && y < 0"); 
+if (quarter == 4) Console.WriteLine("x > 0 && y < 0");
+
