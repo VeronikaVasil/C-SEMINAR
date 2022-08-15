9430834270f06b55116fa67e720d3a3f4ebe4542
Veronika <valeria14081981@yandex.com>
2022-08-15 22:35:39 +0300

    Вариант числового решения сколько цифр в заданном числе

---
+++ Seminar4_2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿//26
+
+Console.WriteLine("Введите число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int GetCountNumbers(int number)
+{
+    int count = 0; // счетчик
+    while (number > 0)
+    {
+        count++;//count = count + 1
+        number /= 10; // Деление на 10
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество чисел: {GetCountNumbers(num)}");
