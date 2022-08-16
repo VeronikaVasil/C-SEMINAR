7ddce87f572b06d51dae86e7a1ff29cd25973e0f
Veronika <valeria14081981@yandex.com>
2022-08-16 14:41:32 +0300

    Factorial from 1 to N

---
+++ Seminar4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает число N и выдает 
+// произведение чисел от 1 до N
+
+Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+
+for (int i = 1; i <= N; i++)
+{
+  factorial = factorial * i;
+}
+Console.WriteLine($"факториал = {factorial}");
+
+
