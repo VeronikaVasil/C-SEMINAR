269cf894278ff83a23730f92215d2e38bcbe5d04
Veronika <valeria14081981@yandex.com>
2022-08-16 22:28:11 +0300

    ДЗ Возведение числа А в степень В

---
+++ Seminar4_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+{
+    int result = Convert.ToInt32(Math.Pow(numberA, numberB));
+
+    Console.WriteLine($"{numberA} ^ {numberB} = {result}");
+}
+Console.ReadLine();
+
