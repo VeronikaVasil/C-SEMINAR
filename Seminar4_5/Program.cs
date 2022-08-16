cac9dcb8a5997643c5dbcfa010a3c894466afbe5
Veronika <valeria14081981@yandex.com>
2022-08-16 20:11:11 +0300

    От 1 до N. Факториал

---
+++ Seminar4_5/Program.cs
@@ -0,0 +1,15 @@
+﻿// 28 Ввести N и найти факториал
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetMultiply (int number)
+{
+    int result = 1; // 2*0 = 2; при умножении указываем 1: любое число на 1 = число
+    // Multiply[1;N]
+    for (int i = 1; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+Console.WriteLine($"Факториал от {num} = {GetMultiply(num)}");
