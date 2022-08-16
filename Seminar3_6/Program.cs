cb6be4d225fef100a92cd2642b77e4f3ee636ae5
Veronika <valeria14081981@yandex.com>
2022-08-16 23:32:07 +0300

    Palindrom

--- Seminar3_6/Program.cs
+++ Seminar3_6/Program.cs
@@ -4,12 +4,12 @@ Console.Write("Введите пятизначное число: ");
 
 string? number = Console.ReadLine();
 
-if (number[0]==number[4] || number[1]==number[3])
-  
-  {
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
-  }
+if (number[0] == number[4] && number[1] == number[3])
 
-  else Console.WriteLine($"Ваше число: {number} - не является палиндромом.");
+{
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+}
+
+else Console.WriteLine($"Ваше число: {number} - не является палиндромом.");
 
 
