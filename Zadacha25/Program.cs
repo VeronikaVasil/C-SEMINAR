5f060b95dadd039ff8f62a76cb98dfcc74b3fed4
Veronika <valeria14081981@yandex.com>
2022-08-19 12:26:16 +0300

    Возаедение введённого числа A в степень введённого чмсла B

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int step = numberA;
+
+for(int i = 1; i < numberB; i++)
+{
+step = step * numberA;
+}
+
+Console.WriteLine($"{numberA} ^ {numberB} = {+ step}");
+
