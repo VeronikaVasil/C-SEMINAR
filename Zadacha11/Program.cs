e6d4c80c190188e407adefdd25e3bfadf634a7c7
Veronika <valeria14081981@yandex.com>
2022-08-21 03:43:34 +0300

    1 и 3 цифры трёхзначного числа через подсчёт

---
+++ Zadacha11/Program.cs
@@ -0,0 +1,5 @@
+﻿// Получить первую и третью цифру трехзначного числа
+
+int number = Convert.ToInt32(Console.ReadLine());
+int result = (number / 100) * 10 + number % 10;
+Console.WriteLine($"Первая и третья цифра числа равна {result}");
