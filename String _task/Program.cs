cf1317ac27e4cb60a04ec3a846633b74606321d0
Veronika <valeria14081981@yandex.com>
2022-08-22 14:28:42 +0300

    Подсчёт символов во введённой строке

---
+++ String _task/Program.cs
@@ -0,0 +1,9 @@
+﻿// Подсчёт символов во введённой строке
+string number = Console.ReadLine();
+int size = number.Length;
+if (number.Length < 3)
+ {
+    Console.WriteLine("Наша строчка не имеет 3 символа");
+ }
+ Console.Write($"Символов в строке {size}");
+ 
