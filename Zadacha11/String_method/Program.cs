ce76907535fa4f88c324f7a87200981ee5eb023c
Veronika <valeria14081981@yandex.com>
2022-08-22 13:53:06 +0300

    1 и 3 цифры числа.Решение строчкой

--- Zadacha11/String_method/Program.cs
+++ Zadacha11/String_method/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Получить первую и третью цифру трехзначного числа
 // решение не счётом, а через строку
+
+Console.Write("Введите трёхзначное число: ");
 string number = Console.ReadLine();
 Console.WriteLine(number[0] + " " + number[2]);
+
