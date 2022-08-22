e585424fb74599632b0a7694ae7e31cb01c9bbe3
Veronika <valeria14081981@yandex.com>
2022-08-22 11:54:27 +0300

    Существует ли треугольник с введёнными 3 сторонами

---
+++ Seminar1_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+
+Console.Write("Введите 1 число : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2 число : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 3 число : ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < number2 + number3 && number2 < number1 + number3 && number3 < number1 + number2)
+{
+  Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
+
