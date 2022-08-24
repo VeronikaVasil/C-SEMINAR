85034fb08dd2c39cb391acd779d78893e7cce33d
Veronika <valeria14081981@yandex.com>
2022-08-24 12:02:08 +0300

    Treugolnik

---
+++ Storon_treug/Program.cs
@@ -0,0 +1,8 @@
+﻿int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+if (A + B > C && A + C > B && B + C > A)
+{
+    Console.WriteLine($"Треугольник со сторонами {A} , {B} , {C} существует ");
+}
+else Console.WriteLine($"Треугольник со сторонами {A} , {B} , {C} НЕ существует ");
