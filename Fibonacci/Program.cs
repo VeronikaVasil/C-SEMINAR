8ee484cc836b8af531dcc6abba795b9e3fa0967e
Veronika <valeria14081981@yandex.com>
2022-08-24 11:45:29 +0300

    Написать числа Фибоначчи, если первые два числа: 0 и 1

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 41: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+//Первые два числа Фибоначчи: 0 и 1.
+//Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+int countFibonacci = 10;
+int firstElement = 0;
+int secondElement = 1;
+Console.WriteLine(firstElement);
+Console.WriteLine(secondElement);
+for (int i = 3; i < countFibonacci; i++)
+{
+    int nextElement = firstElement + secondElement;
+    Console.WriteLine(nextElement);
+    firstElement = secondElement;
+    secondElement = nextElement;
+
+}
