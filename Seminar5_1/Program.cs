cb9dbf85d8ff163da5bc1c0fb18e79a929ed6eff
Veronika <valeria14081981@yandex.com>
2022-08-25 19:20:59 +0300

    Найден или не найден введённый элемент в массиве

---
+++ Seminar5_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] testArray = {-2,-1,0,1,2};
+int searchElement = Convert.ToInt32(Console.ReadLine());
+
+string foundResult = "не найден"; // false - element not found
+for (int i = 0; i < testArray.Length; i++)
+{
+    if (testArray[i] == searchElement) 
+    {
+        foundResult = "найден"; // element found
+        break;
+    }
+}
+Console.WriteLine($"Элемент {searchElement}: {foundResult}");
+// if (foundResult == "найден") Console.WriteLine($"Элемент {searchElement} присутствует ");
+//  //foundResult == true => foundResult
+// else Console.WriteLine($"Элемент {searchElement} ОТСУТСТВУЕТ ");
