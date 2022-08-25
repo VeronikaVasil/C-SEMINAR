bb3e31a4a332900e066974a66d7e49b0264c293d
Veronika <valeria14081981@yandex.com>
2022-08-25 19:42:36 +0300

    Cколько чисел лежит в заданном диапазоне

---
+++ Seminar5_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] testArray = { -2, -1, 0, 1, 2, 15, 22, 99, 100 };
+
+int count = 0; // 0 чисел лежат в диапазоне
+
+for (int i = 0; i < testArray.Length; i++) // testArray.Length - длина массива
+{
+    if (testArray[i] >= 10 && testArray[i] <= 99) count++; // count = count + 1 
+}
+
+System.Console.WriteLine(count);
+
