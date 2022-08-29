f2a49db55a9fa958df6aab01def70cc26ec9584f
Veronika <valeria14081981@yandex.com>
2022-08-29 14:43:49 +0300

    Метод Count

---
+++ Method_count/Program.cs
@@ -0,0 +1,9 @@
+﻿// В следующем примере кода показано, как использовать Count<TSource>(IEnumerable<TSource>) для подсчета элементов в массиве.
+
+string[] fruits = { "apple", "banana", "mango", "orange", "passionfruit", "grape" };
+int numberOfFruits = fruits.Count();
+Console.WriteLine("There are {0} fruits in the collection.", numberOfFruits);
+
+
+
+
